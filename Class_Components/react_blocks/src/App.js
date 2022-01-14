49ebbdf05cc5b3e67f21aa8cdc08530a422cf75d
Augustin Kim <austinkim14@gmail.com>
2022-01-14 10:47:37 -0800

    initial

---
+++ Class_Components/react_blocks/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Main from './components/Main';
+import SubContents from './components/SubContents';
+import Advertisement from './components/Advertisement';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <div className="flex">
+        <Navigation />
+        <Main>
+          <div className="flex">
+            <SubContents />
+            <SubContents />
+            <SubContents />
+          </div>
+          <Advertisement />
+        </Main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
