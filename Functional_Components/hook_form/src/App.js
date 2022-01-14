6da76e557c6c856ebf72efdc60be2f4ad877044d
Augustin Kim <austinkim14@gmail.com>
2022-01-14 12:08:06 -0800

    initial

---
+++ Functional_Components/hook_form/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import UserForm from './components/UserForm';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="card">
+          <div className="card-body">
+            <UserForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
