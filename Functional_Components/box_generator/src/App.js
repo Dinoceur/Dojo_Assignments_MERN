525f7cd9ae7d024e36ee593ee08b9f94f7962d8c
Augustin Kim <austinkim14@gmail.com>
2022-01-16 22:33:28 -0800

    initial

---
+++ Functional_Components/box_generator/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import BoxForm from './components/BoxForm'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="text-center">Box Generator</h1>
+        <BoxForm />
+      </div>
+    </div>
+  );
+}
+
+export default App;
