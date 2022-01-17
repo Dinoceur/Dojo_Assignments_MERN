9ca390b8906ab9c5048c5af92177005eeefe93fd
Augustin Kim <austinkim14@gmail.com>
2022-01-16 22:55:15 -0800

    initial

---
+++ Functional_Components/tabs/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Tabs from './components/Tabs';
+
+function App() {
+  return(
+    <div className="card bg-secondary">
+      <Tabs/>
+    </div>
+  );
+}
+
+export default App;
