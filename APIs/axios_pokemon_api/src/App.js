35816fec8dc20a0c533de68d46cee28311d9d5bd
Augustin Kim <austinkim14@gmail.com>
2022-01-18 22:05:24 -0800

    initial

---
+++ APIs/axios_pokemon_api/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PokemonAPI from './components/PokemonAPI';
+
+function App() {
+
+  return (
+    <div className="container">
+      <PokemonAPI />
+    </div>
+  );
+}
+
+export default App;
